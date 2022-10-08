525036226da358a2744f2c547216ccb32c82fa79
Braxton Fair <hello@taxborn.com>
2022-10-08 18:02:43 -0500

    Improve command line parsing for the compiler

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
 #![allow(dead_code)]
+use clap::Parser;
 use lexer::Lexer;
-use std::{env, fs};
+use std::fs;
+use std::path::PathBuf;
 
 mod lexer;
 
+#[derive(Parser, Debug)]
+#[command(name = "Penguin Compiler")]
+#[command(about = "A compiler for the Penguin programming language", long_about = None)]
+#[command(version = "0.0.1")]
+struct Args {
+    #[arg(short, long, value_name = "FILE")]
+    file: PathBuf,
+}
+
 fn main() {
-    let file = env::args().nth(1).unwrap();
+    let args = Args::parse();
+    let file = args.file;
+
     let contents = fs::read_to_string(&file).unwrap();
 
     let mut lexer = Lexer::new(contents);
